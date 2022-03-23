f22d7f20d596ac7e69ecb48e24bcfb2d25030ce7
kim serin <kimsealin@naver.com>
2022-03-24 00:44:37 +0900

    spring security 진입 설정

---
+++ photogram/src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity //해당 파일로 시큐리티를 활성화
+@Configuration //IoC     
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// super 삭제 -> 기존 시큐리티가 가지고 있는 기능이 다 비활성화 된다.
+		// 인증이 되지 않는 사용자는 모두 로그인 페이지로 이동하도록
+		http.authorizeRequests()
+			.antMatchers("/","/user/**","image/**","/subscribe/**","/comment/**").authenticated() //해당 url은 인증을 받아야 하고
+			.anyRequest().permitAll() //나머지 요청은 모두 허용한다
+			.and()
+			.formLogin()
+			.loginPage("/auth/signin") // 인증되지 않는 사용자가 위의 url에 접근하면 해당 페이지로 이동하도록 하자 
+			.defaultSuccessUrl("/"); //로그인이 정상적으로 처리되면 /로 이동
+	}
+}
