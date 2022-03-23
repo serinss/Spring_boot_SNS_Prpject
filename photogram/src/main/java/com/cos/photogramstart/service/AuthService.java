90e7cd184f195e6ea54973b164f18d657203cad7
kim serin <kimsealin@naver.com>
2022-03-24 01:45:44 +0900

    password 암호화하여 DB에 추가

--- photogram/src/main/java/com/cos/photogramstart/service/AuthService.java
+++ photogram/src/main/java/com/cos/photogramstart/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.cos.photogramstart.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,10 +14,16 @@ import lombok.RequiredArgsConstructor;
 public class AuthService {
 	
 	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 	
-	@Transactional //Write(Insert, Update, Delete) 할때엔느 걸어주자
+	
+	@Transactional //Write(Insert, Update, Delete) 할때에는 걸어주자
 	public User 회원가입(User user) {
-		//회원가입 진행
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword); //암호화 된 패스워드
+		user.setPassword(encPassword);
+		user.setRole("ROLE_USER"); //관리자는 ROLE_ADMIN
+		
 		User userEntity = userRepository.save(user); //외부 데이터가 DB에 들어간 뒤 응답받기
 		return userEntity;
 	}
