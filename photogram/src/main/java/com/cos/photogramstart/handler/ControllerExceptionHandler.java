fae83f4e944abe0bdb98106badc52e87b6637f9a
kim serin <kimsealin@naver.com>
2022-03-27 14:44:25 +0900

    회원정보 수정 - ExceptionHandler

--- photogram/src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ photogram/src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -2,10 +2,13 @@ package com.cos.photogramstart.handler;
 
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cos.photogramstart.handler.ex.CustomValidationApiException;
 import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.util.Script;
 import com.cos.photogramstart.web.dto.CMRespDto;
@@ -13,13 +16,25 @@ import com.cos.photogramstart.web.dto.CMRespDto;
 @RestController
 @ControllerAdvice
 public class ControllerExceptionHandler {
+	//CMRespDto, Script 비교
+	//1. 클라이언트에게 응답할 때는 Script 좋음
+	//2. Ajax 통신 시, CMRespDto
+	//3. Android 통신, CMRespDto
 	
+	// 잘 구분해서 만들어놓자
+	
+	
+	//1. 클라이언트에게 응답할 때는 JavaScript 리턴
 	@ExceptionHandler(CustomValidationException.class) //RuntimeException이 발생하는 모든 클래스를 핸들
 	public String validationException(CustomValidationException e) {
-		//CMRespDto, Script 비교
-		//1. 클라이언트에게 응답할 때는 Script 좋음
-		//2. Ajax 통신 시, CMRespDto
-		//3. Android 통신, CMRespDto
+
 		return Script.back(e.getErrorMap().toString());
+	}
+	
+	//2. Ajax 통신 시, CMRespDto 리턴
+	@ExceptionHandler(CustomValidationApiException.class) //RuntimeException이 발생하는 모든 클래스를 핸들
+	public ResponseEntity<?> validationException(CustomValidationApiException e) {
+		//실패니까 -1
+		return new ResponseEntity<>(new CMRespDto<>(-1, e.getMessage(), e.getErrorMap()), HttpStatus.BAD_REQUEST);
 	}
 }
--- photogram/src/main/java/com/cos/photogramstart/web/UserController.java
+++ photogram/src/main/java/com/cos/photogramstart/web/UserController.java
@@ -2,6 +2,7 @@ package com.cos.photogramstart.web;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -18,6 +19,8 @@ public class UserController {
 	@GetMapping("/user/{id}/update")
 	public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
 		//System.out.println("세션 정보:"+principalDetails.getUser());
+		//이 세션 정보를 Model에 담아서 jsp페이지로 넘어가야 함
+		
 		return "user/update";
 	}
 	
