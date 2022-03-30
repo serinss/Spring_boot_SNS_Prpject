f2be4957febbbd679740a858d12d81172d2d56ac
kim serin <kimsealin@naver.com>
2022-03-31 01:34:04 +0900

    하트 누르기 ajax 기능

--- photogram/src/main/resources/static/js/story.js
+++ photogram/src/main/resources/static/js/story.js
@@ -46,12 +46,20 @@ function getStoryItem(image) {
 	<div class="sl__item__contents">
 		<div class="sl__item__contents__icon">
 
-			<button>
-				<i class="fas fa-heart active" id="storyLikeIcon-${image.id}" onclick="toggleLike(${image.id})"></i>
+			<button>`;
+			
+				if(image.likeState){
+					item+=`<i class="fas fa-heart active" id="storyLikeIcon-${image.id}" onclick="toggleLike(${image.id})"></i>`
+				}else{
+					item+=`<i class="far fa-heart" id="storyLikeIcon-${image.id}" onclick="toggleLike(${image.id})"></i>`
+				}
+				
+				
+			item+=`
 			</button>
 		</div>
 
-		<span class="like"><b id="storyLikeCount-1">3 </b>likes</span>
+		<span class="like"><b id="storyLikeCount-${image.id}">${image.likeCount}</b>likes</span>
 
 		<div class="sl__item__contents__content">
 			<p>${image.caption}</p>
@@ -93,7 +101,7 @@ $(window).scroll(() => {
 	let checkNum = $(window).scrollTop() - ($(document).height() - $(window).height());
 	//console.log(checkNum)
 	
-	if(checkNum < 1 && checkNum > -1){
+	if(checkNum < 10 && checkNum > -10){
 		page++;
 		storyLoad();
 	}
@@ -103,14 +111,46 @@ $(window).scroll(() => {
 // (3) 좋아요, 안좋아요
 function toggleLike(imageId) {
 	let likeIcon = $(`#storyLikeIcon-${imageId}`);
-	if (likeIcon.hasClass("far")) {
-		likeIcon.addClass("fas");
-		likeIcon.addClass("active");
-		likeIcon.removeClass("far");
-	} else {
-		likeIcon.removeClass("fas");
-		likeIcon.removeClass("active");
-		likeIcon.addClass("far");
+	
+	if (likeIcon.hasClass("far")) { //빈 하트 -> 클릭 시, 좋아요
+		
+		$.ajax({
+			type:"post",
+			url:`/api/image/${imageId}/likes`,
+			dataType:"json"
+		}).done(res=>{
+			
+			let likeCountStr = $(`#storyLikeCount-${imageId}`).text(); //해당 아이디의 내부 텍스트값 가져오기
+			let likeCount = Number(likeCountStr)+1;
+			console.log("좋아요 카운트",likeCount)
+			$(`#storyLikeCount-${imageId}`).text(likeCount);
+						
+			likeIcon.addClass("fas");
+			likeIcon.addClass("active");
+			likeIcon.removeClass("far");
+		}).fail(error=>{
+			console.log("오류",error)
+		});
+		
+	} else { //좋아요 취소
+		
+		$.ajax({
+			type:"delete",
+			url:`/api/image/${imageId}/likes`,
+			dataType:"json"
+		}).done(res=>{
+			
+			let likeCountStr = $(`#storyLikeCount-${imageId}`).text();
+			let likeCount = Number(likeCountStr)-1;
+			console.log("좋아요 카운트",likeCount)
+			$(`#storyLikeCount-${imageId}`).text(likeCount);
+			
+			likeIcon.removeClass("fas");
+			likeIcon.removeClass("active");
+			likeIcon.addClass("far");
+		}).fail(error=>{
+			console.log("오류",error)
+		});
 	}
 }
 
