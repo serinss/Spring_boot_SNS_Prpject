851cc1ccebc8dbd9e4ce23a5769acfb7d8407537
kim serin <kimsealin@naver.com>
2022-03-31 01:34:40 +0900

    Image 무한참조 해결

--- photogram/src/main/java/com/cos/photogramstart/domain/image/Image.java
+++ photogram/src/main/java/com/cos/photogramstart/domain/image/Image.java
@@ -1,16 +1,20 @@
 package com.cos.photogramstart.domain.image;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
+import javax.persistence.Transient;
 
-
+import com.cos.photogramstart.domain.likes.Likes;
 import com.cos.photogramstart.domain.user.User;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -29,17 +33,31 @@ public class Image { //N개 - 1명
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	private String caption;
-	private String postImageUrl;
+	private String caption; // 오늘 나 너무 피곤해!!
+	private String postImageUrl; // 사진을 전송받아서 그 사진을 서버에 특정 폴더에 저장 - DB에 그 저장된 경로를 insert
 	
 	@JsonIgnoreProperties({"images"})
-	@ManyToOne
-	@JoinColumn(name = "userId") //FK
-	private User user; //1명 - 1개 이미지
+	@JoinColumn(name = "userId")
+	@ManyToOne(fetch = FetchType.EAGER) // 이미지를 select하면 조인해서 User정보를 같이 들고옴
+	private User user; // 1,  1
+	
+	// 이미지 좋아요
+	@JsonIgnoreProperties({"image"})
+	@OneToMany(mappedBy = "image") 
+	private List<Likes> likes;
+	
+	// 댓글
+	
+	
+	@Transient // DB에 칼럼이 만들어지지 않는다.
+	private boolean likeState;
+	
+	@Transient
+	private int likeCount;
 	
 	private LocalDateTime createDate;
 	
-	@PrePersist //DB에 INSERT되기 직전에 실행
+	@PrePersist
 	public void createDate() {
 		this.createDate = LocalDateTime.now();
 	}
