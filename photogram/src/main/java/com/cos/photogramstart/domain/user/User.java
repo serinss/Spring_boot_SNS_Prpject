9cc1fc971115b6590bc5883e9d722eaea1e9e0ff
kim serin <kimsealin@naver.com>
2022-04-01 01:31:07 +0900

    페이스북 로그인 관련 오류 잡기 - 길이 제한, toString(무한참조)

--- photogram/src/main/java/com/cos/photogramstart/domain/user/User.java
+++ photogram/src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -33,7 +33,7 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //번호 증가 전략이 데이터베이스를 따라간다. (MySQL, Oracle등등)
 	private int id;
 
-	@Column(length=20, unique=true)
+	@Column(length=100, unique=true)
 	private String username;
 	@Column(nullable = false)
 	private String password;
@@ -65,5 +65,15 @@ public class User {
 	public void createDate() {
 		this.createDate = LocalDateTime.now();
 	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", name=" + name + ", website="
+				+ website + ", bio=" + bio + ", email=" + email + ", phone=" + phone + ", gender=" + gender
+				+ ", profileImageUrl=" + profileImageUrl + ", role=" + role +", createDate="
+				+ createDate + "]";
+	}
+	
+	
 	
 }
